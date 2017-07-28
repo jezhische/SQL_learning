fcf8b0bd3549928280fafbd9a82bada9eeb76ed1
jezhische <jezhische@gmail.com>
2017-07-28 19:10:07 +0300

    adding some simple probes

---
+++ simple probes/insert1.sql
@@ -0,0 +1,6 @@
+insert into public.months (id,  "name", "day") values (5, 'january', null);
+insert into public.months (id,  "name") values (12, 'february');
+insert into public.months (id,  "name", "day") values (10, 'january', 18);
+insert into months (id, "name", "day") values (25, 'march', 15);
+insert into months (id, "name", "day") values (36, 'january', 31);
+insert into months (id, "name", "day") values (98, 'may', 15);
